153937cda0f7d251896337b618594186bea0afb7
Gasana14 <gasana141414@gmail.com>
2023-10-23 14:35:59 +0200

    Auth Login and Register

---
+++ src/main/java/com/Library/Management/Rest/APIs/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.Library.Management.Rest.APIs.controllers;
+
+import com.Library.Management.Rest.APIs.dtos.LoginDto;
+import com.Library.Management.Rest.APIs.dtos.RegisterDto;
+import com.Library.Management.Rest.APIs.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+
+
+
+    // Build Login REST API
+    @PostMapping(value = {"/login","signin"})
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+       String response =  authService.login(loginDto);
+       return ResponseEntity.ok(response);
+    }
+
+    // Build Register REST API
+    @PostMapping(value = {"/register","signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+
+
+}
---
+++ src/main/java/com/Library/Management/Rest/APIs/services/AuthService.java
@@ -0,0 +1,79 @@
+package com.Library.Management.Rest.APIs.services;
+
+import com.Library.Management.Rest.APIs.dtos.LoginDto;
+import com.Library.Management.Rest.APIs.dtos.RegisterDto;
+import com.Library.Management.Rest.APIs.exception.LibraryManagementException;
+import com.Library.Management.Rest.APIs.models.Role;
+import com.Library.Management.Rest.APIs.models.User;
+import com.Library.Management.Rest.APIs.repositories.RoleRepository;
+import com.Library.Management.Rest.APIs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    public String login(LoginDto loginDto){
+    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(),loginDto.getPassword()));
+
+    // place authentication into security context holder
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+      return "User Logged in Successfully";
+    }
+
+    public String register(RegisterDto registerDto){
+
+        // check if user with username exist
+        if(userRepository.existsByUsername(registerDto.getUsername())){
+            throw new LibraryManagementException(HttpStatus.BAD_REQUEST,"username "+registerDto.getUsername()+" is already exist.");
+        }
+
+        // check if user with email exist
+        if(userRepository.existsByEmail(registerDto.getEmail())){
+            throw new LibraryManagementException(HttpStatus.BAD_REQUEST,"email "+registerDto.getEmail()+" is already exist.");
+        }
+
+        User user = new User();
+        user.setEmail(registerDto.getEmail());
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        // assign roles to user
+        Set<Role> roles = new HashSet<>();
+        Role role = roleRepository.findByName("ROLE_USER").get();
+
+        roles.add(role);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User is registered successfully";
+    }
+
+
+
+}
